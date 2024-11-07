4ed45c13d7259f933977c550ede94569b9de008e
Jacky Chen <chenjacky32@gmail.com>
2024-11-07 18:34:30 +0700

    add: routes SPA and get data from api movie

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'be4240a0dd23a51c548c3e09529794f2',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
